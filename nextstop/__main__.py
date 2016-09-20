ff87b85501a86bf4438d9708588d68cf6a6d7b43
Pierre-Olivier Mercier <nemunaire@nemunai.re>
2016-09-20 23:58:23 +0200

    Use argparse instead of getopt

--- nextstop/__main__.py
+++ nextstop/__main__.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3
 
-import getopt
+import argparse
 import sys
 
 from . import ratp
@@ -46,60 +46,40 @@ def extractInformation(transport,
           print("Aucune ligne trouvée.")
 
 
-
-def printUsage(name):
-  print("Usage:\t%s -t transport_type -l line [-s station [-d direction]] " % name)
-  print("\t%s -a -t transport_type -c cause" % name)
-  print("\t-h: Display this help")
-  print("\t-t transport_type: transportation type: bus, rer, tram, "
-      + "noctilien, metro")
-  print("\t-l line: line number or name. e.g.: 72, A, T3")
-  print("\t-s station: optionnal: station for which to print the next stops")
-  print("\t-d destination: optionnal: destination for which to print the next stops")
-  print("\t-c cause: cause of the disturbance (alerte, travaux, or manif)")
-  print("\t-a: get alerts and transportation status (work on the line, manifestations)")
-
 def main():
-  type_transp = ''
-  line = None
-  station = ''
-  alert = False
-  cause = ''
-  destination = None
+  opts = argparse.ArgumentParser()
 
-  try:
-    opt, args = getopt.getopt(sys.argv[1:], "aht:l:s:d:c:", ["help"])
-  except getopt.GetoptError:
-    printUsage(sys.argv[0])
-    return 1
-  for op, val in opt:
-    if op in ("-h", "--help"):
-      printUsage(sys.argv[0])
-      return 0
-    elif op == "-t":
-      type_transp = val
-    elif op == "-l":
-      line = val
-    elif op == "-s":
-      station = val
-    elif op == "-a":
-      alert = True
-    elif op == "-c":
-      cause = val
-    elif op == "-d":
-      destination = val
+  opts.add_argument("-t", "--transport-type", "--type",
+                    help="transportation type: bus, rer, tram, noctilien, metro")
 
-  if alert:
-    if type_transp == "" and cause == "":
-      printUsage(sys.argv[0])
+  opts.add_argument("-l", "--line",
+                    help="line number or name. e.g.: 72, A, T3")
+
+  opts.add_argument("-s", "--station", "--stop",
+                    help="station for which to print the next stops (optional)")
+
+  opts.add_argument("-d", "--direction",
+                    help="destination for which to print the next stops (optional)")
+
+  opts.add_argument("-c", "--cause",
+                    help="cause of the disturbance (one of: alerte, travaux, or manif)")
+
+  opts.add_argument("-a", "--alert", action="store_true",
+                    help="get alerts and transportation status (work on the line, manifestations)")
+
+  args = opts.parse_args()
+
+  if args.alert:
+    if args.transport_type is None and args.cause is None:
+      opts.print_help()
       return 1
-    print(ratp.getDisturbance(cause, type_transp))
+    print(ratp.getDisturbance(args.cause, args.transport_type))
     return 0
-  if type_transp == "":
-    printUsage(sys.argv[0])
+  if args.transport_type is None:
+    opts.print_help()
     return 1
 
-  return extractInformation(type_transp, line, station, destination)
+  return extractInformation(args.transport_type, args.line, args.station, args.direction)
 
 if __name__ == "__main__":
       sys.exit(main())
